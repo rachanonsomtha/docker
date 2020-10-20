3915b78f2853e17b734d4e388f08fa5d67693b3d
rachanonsomtha <31127897+rachanonsomtha@users.noreply.github.com>
2020-10-20 13:32:23 +0700

    initial commit

---
+++ php/src/app/Http/Controllers/NewsController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
+
+class NewsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        Redis::set('name', 'Taylor');
+        
+        $user = Redis::get('name');
+
+        $session_id = session()->getId();
+
+        $result = $_SERVER['HOSTNAME'] . " = " . $session_id;
+
+        session()->put('app', 'ford');
+
+        $app_data = session()->get('app');
+
+        $result = $result . " = app=" . $app_data;
+
+        return view('news', ['result'=> $result]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ php/src/resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+<title>Home - Hello World!</title>
+</head>
+<body>
+<h1>Home - Hello World!</h1>
+<p>{{ config('app.name') }}</p>
+</body>
+</html> 
+
+{{ phpinfo() }}
